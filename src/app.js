d6140a8c49a47dfff4acc708449a747c00e92d66
Andrés Vélez <147754935+andresColmancol@users.noreply.github.com>
2024-02-20 14:16:20 -0500

    add more content

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const connect = require("./config/database");
 const configExpress = require("./config/express");
 const routes = require("./routes");
-const { formData } = require("./middlewares/formData");
 
 const app = express();
 connect();
@@ -13,9 +12,6 @@ configExpress(app);
 // Routes
 routes(app);
 
-// Middleware
-app.post("/post-image", formData, (req, res) => {
-  res.status(200).json({ ...req.body });
-});
+
 
 module.exports = app;
